43ab48a78b2d33443ea16b8ccd9fbc9fc6ae7638
tsering.sherpa <tsering.sherpa@info.com.np>
2023-09-26 17:36:31 +0545

    My first commit

---
+++ app/src/main/java/com/example/myfirstapplication/RegisterActivity.java
@@ -0,0 +1,70 @@
+package com.example.myfirstapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    private EditText txtFieldEmail, txtFieldPassword;
+    private Button btnRegister;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        txtFieldEmail = findViewById(R.id.txtFieldEmail);
+        txtFieldPassword = findViewById(R.id.txtFieldPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        auth = FirebaseAuth.getInstance();
+
+        initOnClickListener();
+    }
+
+    private void initOnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = txtFieldEmail.getText().toString();
+                String txt_password = txtFieldPassword.getText().toString();
+
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(RegisterActivity.this, "Empty Credentials !!!", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Password too short !", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(txt_email, txt_password);
+                }
+
+            }
+        });
+    }
+
+    private void registerUser(String _email, String _password) {
+        auth.createUserWithEmailAndPassword(_email, _password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
